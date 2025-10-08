057dd127c9368337b296f9806dc8f30fd1c33be9
SimranTrivedi <simrantrivedi2904@gmail.com>
2025-10-08 20:10:21 +0530

    function learning

---
+++ level1/level1/development/apiserver_1/app/test.py
@@ -0,0 +1,11 @@
+def getJsonObject(name, age):
+    return {
+        "name": name,
+        "age": age
+    }
+
+simobj = getJsonObject("Simran", 20)
+harishobj = getJsonObject("Harish", 21)
+
+print(simobj)
+print(harishobj)
