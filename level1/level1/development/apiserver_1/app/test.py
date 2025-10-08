320aec9ddcca8a987efb33f92b2c8b8f47b845fc
SimranTrivedi <simrantrivedi2904@gmail.com>
2025-10-08 20:16:47 +0530

    even number

--- level1/level1/development/apiserver_1/app/test.py
+++ level1/level1/development/apiserver_1/app/test.py
@@ -1,3 +1,7 @@
+def even_number(num):
+    for i in range(num):
+        if i % 2 == 0:
+            print(i)
 def getJsonObject(name, age):
     return {
         "name": name,
@@ -9,3 +13,5 @@ harishobj = getJsonObject("Harish", 21)
 
 print(simobj)
 print(harishobj)
+my_number=even_number(10)
+print(my_number)
